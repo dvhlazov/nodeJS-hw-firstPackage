cc57f8ea41136c4b37337ce8d18ddac61e1b3342
dvhlazov <dvhlazov@gmail.com>
2024-07-07 16:09:32 +0300

    added dataAndTime

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import chalk from 'chalk';
+import { getCurrentDateTime } from './dateAndTIme.js';
 
 let searchMath = 5+5 + 'years';
 
 console.log(chalk.bgMagenta(searchMath));
+console.log(getCurrentDateTime());
 
---
+++ dateAndTIme.js
@@ -0,0 +1,6 @@
+import  moment  from 'moment';
+
+
+export const getCurrentDateTime = () => {
+    return moment().format('MMMM Do YYYY, h:mm:ss a');
+};
